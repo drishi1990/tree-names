708b701236e6a3fb2fa9bfdd6391cc96beb8af02
Lokesh Yadav <lokesh.yadav@publicissapient.com>
2020-07-08 10:42:51 +0530

    preload fonts & hero image mobile variant

--- packages/beauty-portal-theme/gatsby-ssr.js
+++ packages/beauty-portal-theme/gatsby-ssr.js
@@ -5,6 +5,25 @@ const headComponents = [];
 headComponents.push(
   <link rel="preconnect" href="https://cdn.sanity.io" />,
   <link rel="dns-prefetch" href="https://cdn.sanity.io" />,
+  <link
+    rel="preload"
+    href="https://cdn.sanity.io/images/8azq2eb8/production/2b6b4d1fc5552b58670056e3f4394d3af452ef39-782x439.jpg?rect=0,2,782,434&w=414&h=230&fit=max&auto=format"
+    as="image"
+  />,
+  <link
+    rel="preload"
+    href="/fonts/ProximaNovaAltRegular.woff2"
+    as="font"
+    type="font/woff2"
+    crossOrigin
+  />,
+  <link
+    rel="preload"
+    href="/fonts/ProximaNovaAltBold.woff2"
+    as="font"
+    type="font/woff2"
+    crossOrigin
+  />,
   <link rel="shortcut icon" type="image/x-icon" href="data:image/x-icon;," />,
   <script
     type="text/javascript"
