36e77ad00ba74dacc38268b35478044797b8510d
Lokesh Yadav <lokesh.yadav@publicissapient.com>
2020-07-12 22:42:10 +0530

    newsletter promo module css

--- packages/beauty-portal-theme/src/components/NewsletterPromo/index.tsx
+++ packages/beauty-portal-theme/src/components/NewsletterPromo/index.tsx
@@ -3,26 +3,24 @@ import { Link } from 'gatsby';
 import BlockContent from '@sanity/block-content-to-react';
 import { NewsletterPromoInterface } from './models';
 import { blockTypeDefaultSerializers } from '../../helpers/sanity';
-// import useStyles from './styles';
+import styles from './styles.module.scss';
 
 const NewsletterPromo: FunctionComponent<NewsletterPromoInterface> = ({
   _rawBody,
   ctaLabel,
 }) => {
-  // const classes = useStyles();
-
   return (
-    <section className={'classes.newsletter'}>
-      <div className={'classes.newsletterContent'}>
+    <section className={styles.newsletter}>
+      <div className={styles.newsletterContent}>
         {_rawBody && (
-          <h3 className={'classes.newsletterTitle'}>
+          <h3 className={styles.newsletterTitle}>
             <BlockContent
               serializers={blockTypeDefaultSerializers}
               blocks={_rawBody}
             />
           </h3>
         )}
-        <Link to="/subscribe/" className={'classes.subscribe'}>
+        <Link to="/subscribe/" className={styles.subscribe}>
           {ctaLabel}
         </Link>
       </div>
